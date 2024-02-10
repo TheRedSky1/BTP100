f05e6dfb6b1a08cd5380facb2d194e4f0b6b6153
Jason <79380963+TheRedSky1@users.noreply.github.com>
2024-02-09 20:25:39 -0500

    Update w1p2.c

--- w1p2.c
+++ w1p2.c
@@ -1,3 +1,19 @@
+/*
+
+###########################################################################
+                          Workshop - #1 (P2)
+Full Name  : Jason Zabawskyj
+
+Authenticity Declaration:
+
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+###########################################################################
+
+*/
+
+
 #include <stdio.h>
 
 int main(void) {
